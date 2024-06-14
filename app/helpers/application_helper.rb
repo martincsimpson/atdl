4d9e2c8ffe9507fb58b09cc588efd6abe8711f64
Martin Simpson <martin.simpson@ubiquitypress.com>
2024-06-14 13:44:33 +0700

    Added task status display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,27 @@ module ApplicationHelper
     return parent if parent.is_a?(Project)
     return find_root_project(parent.project) if parent.is_a?(Task)
   end
+
+  def task_status_color(status)
+    case status
+    when 'new'
+      'blue'
+    when 'todo'
+      'orange'
+    when 'doing'
+      'yellow'
+    when 'done'
+      'green'
+    when 'deferred'
+      'purple'
+    when 'delegated'
+      'red'
+    when 'dropped'
+      'grey'
+    else
+      'black'
+    end
+  end
+
 end
+
